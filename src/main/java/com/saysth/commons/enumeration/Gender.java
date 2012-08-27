0c9423a3ecbee956a20e40e329c55bf9e2b07aed
kelvin <zhq.personal@gmail.com>
2012-08-27 16:39:10 +0800

    添加性别枚举

---
+++ src/main/java/com/saysth/commons/enumeration/Gender.java
@@ -0,0 +1,49 @@
+package com.saysth.commons.enumeration;
+
+import java.util.Map;
+
+import org.springframework.util.Assert;
+
+/**
+ * 性别枚举
+ * 
+ * @author KelvinZ
+ * 
+ */
+public enum Gender {
+	UNSPEC("未指定"), // 未指定
+	MALE("男"), // 男
+	FEMALE("女"); // 女
+
+	@SuppressWarnings("rawtypes")
+	private static Map<Enum, String> cache;
+	private String label;
+
+	private Gender(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * 把自身和标签放到缓存，方便用自定义标签处理
+	 * @param cache
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void cache(Map<Enum, String> cache) {
+		Assert.notNull(cache);
+		Gender.cache = cache;
+		Gender[] enums = Gender.values();
+		for (Gender gender : enums)
+			Gender.cache.put(gender, gender.getLabel());
+	}
+
+	// 获取存放缓存
+	@SuppressWarnings("rawtypes")
+	public static Map<Enum, String> getCache() {
+		return Gender.cache;
+	}
+
+}
